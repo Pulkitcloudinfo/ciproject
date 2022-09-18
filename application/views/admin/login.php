9c9f0e5ad71c25706e31d64bc184b903e0c75a9f
Pulkitcloudinfo <105346488+Pulkitcloudinfo@users.noreply.github.com>
2022-09-18 12:11:26 +0530

    Update login.php

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Admin Login</title>
+    <title>ciproject Admin Login</title>
 
 <?php echo link_tag('assests/vendor/bootstrap/css/bootstrap.min.css'); ?>
 <?php echo link_tag('assests/vendor/fontawesome-free/css/all.min.css'); ?>
